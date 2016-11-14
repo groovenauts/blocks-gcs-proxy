9bead06edd8c886f0fa68b12842fd98d2fa9c6be
akm <akm2000@gmail.com>
2016-11-14 18:04:29 +0900

    bundle gem magellan-gcs-proxy --exe --coc --mit --test=rspec

---
+++ lib/magellan/gcs/proxy.rb
@@ -0,0 +1,9 @@
+require "magellan/gcs/proxy/version"
+
+module Magellan
+  module Gcs
+    module Proxy
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/magellan/gcs/proxy/version.rb
@@ -0,0 +1,7 @@
+module Magellan
+  module Gcs
+    module Proxy
+      VERSION = "0.1.0"
+    end
+  end
+end
