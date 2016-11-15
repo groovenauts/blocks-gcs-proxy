d9f8c5a5afa45a38e251aa56cda70d9ee1da9c0a
akm <akm2000@gmail.com>
2016-11-15 11:50:56 +0900

    :recycle: Extract parse_uri method

--- lib/magellan/gcs/proxy/file_operation.rb
+++ lib/magellan/gcs/proxy/file_operation.rb
@@ -17,8 +17,7 @@ module Magellan
         def download(definitions)
           (definitions || []).each do |obj|
             logger.info("Downloading: #{obj.inspect}")
-            uri = URI.parse(obj['src'])
-            raise "Unsupported scheme #{uri.scheme.inspect} in #{obj.inspect}" unless uri.scheme == 'gs'
+            uri = parse_uri(obj['src'])
             bucket = storage.bucket(uri.host)
             file = bucket.file uri.path.sub(/\A\//, '')
             file.download obj['dest']
@@ -28,11 +27,16 @@ module Magellan
         def upload(definitions)
           (definitions || []).each do |obj|
             logger.info("Uploading: #{obj.inspect}")
-            uri = URI.parse(obj['dest'])
-            raise "Unsupported scheme #{uri.scheme.inspect} in #{obj.inspect}" unless uri.scheme == 'gs'
+            uri = parse_uri(obj['dest'])
             bucket = storage.bucket(uri.host)
             bucket.create_file obj['src'], uri.path.sub(/\A\//, '')
           end
+        end
+
+        def parse_uri(str)
+          uri = URI.parse(str)
+          raise "Unsupported scheme #{uri.scheme.inspect} of #{str}" unless uri.scheme == 'gs'
+          uri
         end
 
       end
