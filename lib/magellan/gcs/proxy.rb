9eb21e8d010a8bbd8ea9cf45d4acb4d707577d7d
akm <akm2000@gmail.com>
2016-11-21 17:51:28 +0900

    :shower: Remove pubsub_operation

--- lib/magellan/gcs/proxy.rb
+++ lib/magellan/gcs/proxy.rb
@@ -4,7 +4,6 @@ require 'magellan/gcs/proxy/context'
 require 'magellan/gcs/proxy/expand_variable'
 require 'magellan/gcs/proxy/gcp'
 require 'magellan/gcs/proxy/message_wrapper'
-require 'magellan/gcs/proxy/pubsub_operation'
 
 module Magellan
   module Gcs
