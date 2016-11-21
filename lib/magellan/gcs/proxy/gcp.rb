f1a0071faffedd4a379821a415eda3a5f9d441f5
akm <akm2000@gmail.com>
2016-11-21 18:30:44 +0900

    :+1: Use application defautl credential for GCS

--- lib/magellan/gcs/proxy/gcp.rb
+++ lib/magellan/gcs/proxy/gcp.rb
@@ -11,26 +11,29 @@ module Magellan
 
         module_function
 
-        SCOPES = [
-          "https://www.googleapis.com/auth/devstorage.full_control",
-          "https://www.googleapis.com/auth/pubsub",
-        ].freeze
+        SCOPE_STORAGE = "https://www.googleapis.com/auth/devstorage.full_control".freeze
+        SCOPE_PUBSUB  = "https://www.googleapis.com/auth/pubsub".freeze
+        SCOPES = [SCOPE_STORAGE, SCOPE_PUBSUB].freeze
 
         # See
         #   https://developers.google.com/identity/protocols/application-default-credentials
         #   https://github.com/google/google-auth-library-ruby
         def authorization
-          @authorization ||= Google::Auth.get_application_default(SCOPES)
+          @authorization ||= Google::Auth.get_application_default(SCOPES).tap{|auth| auth.apply({}) }
         end
 
         def storage
           unless @storage
-            @storage = Google::Cloud::Storage.new(
-              # default credential を利用するため、プロジェクトの指定はしない
-              # project: ENV['GOOGLE_PROJECT'] || 'dummy-project-id',
-              # keyfile: ENV['GOOGLE_KEY_JSON_FILE'],
-            )
-            @storage.service.authorization = authorization
+            # @storage = Google::Cloud::Storage.new(
+            #   # default credential を利用するため、プロジェクトの指定はしない
+            #   # project: ENV['GOOGLE_PROJECT'] || 'dummy-project-id',
+            #   # keyfile: ENV['GOOGLE_KEY_JSON_FILE'],
+            # )
+            credentials = Google::Cloud::Storage::Credentials.default scope: SCOPE_STORAGE
+            service = Google::Cloud::Storage::Service.new(nil, credentials, retries: nil, timeout: nil)
+            impl = service.instance_variable_get(:@service)
+            impl.authorization = authorization
+            @storage = Google::Cloud::Storage::Project.new(service)
           end
           @storage
         end
