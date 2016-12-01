48abb3735cad0403938054b095a407c88319d232
akm <akm2000@gmail.com>
2016-12-01 20:54:03 +0900

    :bug: Fix load error

--- lib/magellan/gcs/proxy.rb
+++ lib/magellan/gcs/proxy.rb
@@ -2,17 +2,19 @@ require 'dotenv'
 Dotenv.load
 
 require "magellan/gcs/proxy/version"
-require 'magellan/gcs/proxy/cli'
-require 'magellan/gcs/proxy/composite_logger'
-require 'magellan/gcs/proxy/config'
-require 'magellan/gcs/proxy/context'
 require 'magellan/gcs/proxy/expand_variable'
-require 'magellan/gcs/proxy/gcp'
+require 'magellan/gcs/proxy/config'
 require 'magellan/gcs/proxy/log'
-require 'magellan/gcs/proxy/message_wrapper'
-require 'magellan/gcs/proxy/progress_notification'
+require 'magellan/gcs/proxy/gcp'
+
+require 'magellan/gcs/proxy/composite_logger'
 require 'magellan/gcs/proxy/progress_notifier_adapter'
 require 'magellan/gcs/proxy/pubsub_progress_notifier'
+require 'magellan/gcs/proxy/progress_notification'
+
+require 'magellan/gcs/proxy/message_wrapper'
+require 'magellan/gcs/proxy/context'
+require 'magellan/gcs/proxy/cli'
 
 module Magellan
   module Gcs
