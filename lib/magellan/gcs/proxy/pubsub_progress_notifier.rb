f3667b786121742b68f3c2613cf20937649b66ef
akm <akm2000@gmail.com>
2016-12-05 12:14:56 +0900

    :+1: Extract #topic to build topic object from topic_name

--- lib/magellan/gcs/proxy/pubsub_progress_notifier.rb
+++ lib/magellan/gcs/proxy/pubsub_progress_notifier.rb
@@ -7,9 +7,13 @@ module Magellan
   module Gcs
     module Proxy
       class PubsubProgressNotifier
-        attr_reader :topic
-        def initialize(topic)
-          @topic = topic
+        attr_reader :topic_name
+        def initialize(topic_name)
+          @topic_name = topic_name
+        end
+
+        def topic
+          @topic ||= GCP.pubsub.topic(topic_name)
         end
 
         def notify(severity, job_message, data, attrs)
