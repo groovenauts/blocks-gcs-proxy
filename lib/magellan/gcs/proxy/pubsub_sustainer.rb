a96bc429d390e4525c552e89f353c26d6a54b227
akm <akm2000@gmail.com>
2016-12-14 16:24:10 +0900

    :shirt: Fix indentation

--- lib/magellan/gcs/proxy/pubsub_sustainer.rb
+++ lib/magellan/gcs/proxy/pubsub_sustainer.rb
@@ -43,13 +43,13 @@ module Magellan
               logger.debug("#{self.class.name} is stopping.")
               break
             end
-              logger.debug("#{self.class.name} Sending delay!(#{delay})")
-              message.delay! delay
-              logger.debug("#{self.class.name} Sent delay!(#{delay}) successfully")
+            logger.debug("#{self.class.name} Sending delay!(#{delay})")
+            message.delay! delay
+            logger.debug("#{self.class.name} Sent delay!(#{delay}) successfully")
           end
           logger.debug("#{self.class.name} stopped.")
-      rescue => e
-        logger.error(e)
+        rescue => e
+          logger.error(e)
         end
 
         def wait_while_processing(seconds)
