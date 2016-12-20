5671f47d0eb316f4aa8d1ecf7612c9366e37968d
akm <akm2000@gmail.com>
2016-12-20 15:10:32 +0900

    :shirt: bundle exec rubocop --auto-correct

--- spec/example/app.sh_spec.rb
+++ spec/example/app.sh_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 require 'fileutils'
 require 'tmpdir'
 
-describe "example/app.sh" do
+describe 'example/app.sh' do
   let(:command_path) { File.expand_path('../../../example/app.sh', __FILE__) }
   let(:test_file_path) { 'bucket1/foo/bar/baz.txt' }
   let(:test_suffix) { 'test' }
@@ -16,16 +16,16 @@ describe "example/app.sh" do
     FileUtils.mkdir_p(downloads_dir)
     FileUtils.mkdir_p(uploads_dir)
   end
-  after {  FileUtils.remove_entry_secure tmpdir }
+  after { FileUtils.remove_entry_secure tmpdir }
 
-  context "valid" do
+  context 'valid' do
     it do
       download_file = File.join(downloads_dir, test_file_path)
       FileUtils.mkdir_p(File.dirname(download_file))
       open(download_file, 'w') { |f| f.puts(test_content) }
       r = system([command_path, download_file, downloads_dir, uploads_dir, test_suffix].join(' '))
       expect(r).to be_truthy
-      result_path = File.join(uploads_dir, test_file_path.sub(%r{\.txt\z}, "-#{test_suffix}.txt"))
+      result_path = File.join(uploads_dir, test_file_path.sub(/\.txt\z/, "-#{test_suffix}.txt"))
       expect(File.read(result_path).strip).to eq test_content
     end
   end
