844de39489a3e2c2657e8bc1cc6106a086435453
akm <akm2000@gmail.com>
2016-12-20 15:06:55 +0900

    :+1: Show local variables in example/app.sh

--- example/app.sh
+++ example/app.sh
@@ -21,12 +21,27 @@ dl_relpath=${dl_localpath##$dl_bucket/}
 dl_fname=${dl_relpath##*/}
 dl_dir=${dl_relpath%$dl_fname}
 
+echo "dl_localpath=${dl_localpath}"
+echo "dl_bucket=${dl_bucket}"
+echo "dl_relpath=${dl_relpath}"
+echo "dl_fname=${dl_fname}"
+echo "dl_dir=${dl_dir}"
+
+
 fname="${fpath##*/}"
 fbase="${fname%.*}"
 fext="${fpath##*.}"
 
+echo "fname=${fname}"
+echo "fbase=${fbase}"
+echo "fext=${fext}"
+
+
 ul_dir="$3/${dl_bucket}/${dl_dir}"
 ul_path="${ul_dir}${fbase}-${suffix}.${fext}"
+
+echo "ul_dir=${ul_dir}"
+echo "ul_path=${ul_path}"
 
 mkdir -p $ul_dir
 cp $fpath $ul_path
