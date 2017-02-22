460715a4220e8f6c39e4a5fff68681182d93e6c9
akm <akm2000@gmail.com>
2017-02-22 22:14:38 +0900

    :+1: Implementing downloadFiles and uploadFiles...

--- job.go
+++ job.go
@@ -1,8 +1,12 @@
 package gcsproxy
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"golang.org/x/net/context"
@@ -16,7 +20,7 @@ type (
 		Commands map[string][]string
 		Dryrun bool
 	}
-	
+
 	Job struct {
 		config *JobConfig
 		message *pubsub.ReceivedMessage
@@ -25,16 +29,51 @@ type (
 )
 
 func (job *Job) execute(ctx context.Context) error {
-	cmd, err := job.build(ctx)
+	return job.setupWorkspace(ctx, func(workspace, downloads_dir, uploads_dir string) error {
+		_, err := job.downloadFiles(downloads_dir)
+		if err != nil {
+			return err
+		}
+
+		cmd, err := job.build(ctx)
+		if err != nil {
+			log.Printf("Command build Error template: %v msg: %v cause of %v\n", job.config.Template, job.message, err)
+			return err
+		}
+		err = cmd.Run()
+		if err != nil {
+			log.Printf("Command Error: cmd: %v cause of %v\n", cmd, err)
+			// return err // Don't return this err
+		}
+
+		err = job.uploadFiles(uploads_dir)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func (job *Job) setupWorkspace(ctx  context.Context, f func(workspace, downloads_dir, uploads_dir string) error) error {
+	dir, err := ioutil.TempDir("", "workspace")
 	if err != nil {
-		log.Printf("Command build Error template: %v msg: %v cause of %v\n", job.config.Template, job.message, err)
+		log.Fatal(err)
 		return err
 	}
-	err = cmd.Run()
-	if err != nil {
-		log.Printf("Command Error: cmd: %v cause of %v\n", cmd, err)
+	defer os.RemoveAll(dir) // clean up
+
+	subdirs := []string{
+		filepath.Join(dir, "downloads"),
+		filepath.Join(dir, "uploads"),
 	}
-	return nil
+	for _, subdir := range subdirs {
+		err := os.MkdirAll(subdir, 0700)
+		if err != nil {
+			return err
+		}
+	}
+	return f(dir, subdirs[0], subdirs[1])
 }
 
 func (job *Job) build(ctx context.Context) (*exec.Cmd, error) {
@@ -64,4 +103,69 @@ func (job *Job) extract(ctx context.Context, values []string) ([]string, error) 
 		result = append(result, extracted)
 	}
 	return result, nil
+}
+
+func (job *Job) downloadFiles(dir string) (map[string]string, error) {
+	result := map[string]string{}
+	objects := job.flatten(job.parseJson(job.Message.Attributes["download_files"]))
+	remote_files := []string{}
+	for _, obj := range objects {
+		switch obj.(type) {
+		case string:
+			remote_files = append(remote_files, obj.(string))
+		default:
+			log.Printf("Invalid download file URL: %v [%T]", obj, obj)
+		}
+	}
+
+	return result, nil
+}
+
+func (job *Job) uploadFiles(dir string) error {
+	return nil
+}
+
+func (job *Job) parseJson(source string) interface{} {
+	matched, err := regexp.MatchString(`\A\[.*\]\z|\A\{.*\}\z|`, str)
+	if err != nil {
+		return str
+	}
+	if !matched {
+		return str
+	}
+	var dest interface{}
+	err = json.Unmarshal([]byte(str), &dest)
+	if err != nil {
+		return str
+	}
+	return dest
+}
+
+
+func (job *Job) flatten(obj interface{}) []interface{} {
+	// Support only unmarshalled object from JSON
+	// See https://golang.org/pkg/encoding/json/#Unmarshal also
+	switch obj.(type) {
+	case []interface{}:
+		res := []interface{}{}
+		for _, i := range obj {
+			switch i.(type) {
+			case bool, float64, string, nil:
+				res = append(res, i)
+			default:
+				for _, j := range job.flatten(i) {
+					res = append(res, j)
+				}
+			}
+		}
+		return res
+	case map[string]interface{}:
+		values := []interface{}
+		for _, val := range obj {
+			values = append(values, val)
+		}
+		return job.flatten(values)
+	default:
+		return []interface{}{obj}
+	}
 }
