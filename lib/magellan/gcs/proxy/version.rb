a8efac02ad5e72bfef7dbab557dc10f0e0a3f066
akm <akm2000@gmail.com>
2017-02-22 10:33:36 +0900

    :shower: Remove ruby code

--- lib/magellan/gcs/proxy/version.rb
+++
@@ -1,7 +0,0 @@
-module Magellan
-  module Gcs
-    module Proxy
-      VERSION = '0.3.2'.freeze
-    end
-  end
-end
--- spec/magellan/gcs/proxy_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Magellan::Gcs::Proxy do
-  it 'has a version number' do
-    expect(Magellan::Gcs::Proxy::VERSION).not_to be nil
-  end
-end
