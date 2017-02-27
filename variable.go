4c42a048032faa39aa0f5396ad132a885ca5baab
akm <akm2000@gmail.com>
2017-02-27 16:14:24 +0900

    :+1: Implement Variable.digIn to parse JSON

--- variable.go
+++ variable.go
@@ -1,6 +1,7 @@
 package gcsproxy
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
 	"strconv"
@@ -18,7 +19,7 @@ func (v *Variable)expand(str string) (string, error) {
 	re0 := regexp.MustCompile(`\%\{\s*([\w.]+)\s*\}`)
 	re1 := regexp.MustCompile(`\A\%\{\s*`)
 	re2 := regexp.MustCompile(`\s*\}\z`)
-	res := re0.ReplaceAllStringFunc("A/%{  foo  }/B/%{bar}/D", func(raw string) string{
+	res := re0.ReplaceAllStringFunc(str, func(raw string) string{
 		expr := re1.ReplaceAllString(re2.ReplaceAllString(raw, ""), "")
 		value, err := v.dive(expr)
 		if err != nil {
@@ -40,8 +41,10 @@ func (v *Variable)expand(str string) (string, error) {
 
 func (v *Variable)dive(expr string) (interface{}, error) {
 	var_names := strings.Split(expr, expr_separator)
+	// fmt.Printf("var_names: %v\n", var_names)
 	res, err := v.inject(var_names, v.data, func(tmp interface{}, name string) (interface{}, error) {
 		res, err := v.dig(tmp, name, expr)
+		// fmt.Printf("res: %v err: %v\n", res, err)
 		if err != nil { return nil, err }
 		return res, nil
 	})
@@ -50,30 +53,56 @@ func (v *Variable)dive(expr string) (interface{}, error) {
 }
 
 func (v *Variable)dig(tmp interface{}, name, expr string) (interface{}, error) {
-	matched, err := regexp.MatchString(`\A\d+\z`, name)
-	if err != nil {
-		return nil, err
-	}
-	if matched {
-		idx, err := strconv.Atoi(name)
-		if err != nil { return nil, err }
-		switch tmp.(type) {
-		case []string:
-			return tmp.([]string)[idx], nil
-		case []interface{}:
-			return tmp.([]interface{})[idx], nil
-		case map[string]interface{}:
-			return tmp.(map[string]interface{})[name], nil
+	result, err := v.digIn(tmp, name, expr)
+	if err != nil { return nil, err }
+	// fmt.Printf("name: %v, result: %v\n", name, result)
+	switch result.(type) {
+	case string:
+		re := regexp.MustCompile(`\A\{.*\}\z|\A\[.*\]\z`)
+		matched := re.MatchString(result.(string))
+		if matched {
+			var obj interface{}
+			src := []byte(result.(string))
+			err := json.Unmarshal(src, &obj)
+			if err != nil {
+				return result.(string), nil
+			}
+			return obj, nil
+		} else {
+			return result, nil
 		}
-	} else {
-		switch tmp.(type) {
-		case map[string]interface{}:
-			return tmp.(map[string]interface{})[name], nil
-		}
+	default:
+		return result, nil
 	}
-	return nil, fmt.Errorf("Invalid Reference")
 }
 
+func (v *Variable)digIn(tmp interface{}, name, expr string) (interface{}, error) {
+	switch tmp.(type) {
+	case []string:
+		idx, err := v.parseIndex(name)
+		if err != nil {return nil, err}
+		return tmp.([]string)[idx], nil
+	case []interface{}:
+		idx, err := v.parseIndex(name)
+		if err != nil {return nil, err}
+		return tmp.([]interface{})[idx], nil
+	case map[string]interface{}:
+		return tmp.(map[string]interface{})[name], nil
+	default:
+		return nil, fmt.Errorf("Unsupported Object type: [%T]%v", tmp, tmp)
+	}
+}
+
+func (v *Variable) parseIndex(str string) (int, error) {
+	re := regexp.MustCompile(`\A\d+\z`)
+	matched := re.MatchString(str)
+	if !matched {
+		return 0, fmt.Errorf("Invalid Reference: %v", str)
+	}
+	idx, err := strconv.Atoi(str)
+	if err != nil { return 0, err }
+	return idx, nil
+}
 
 func (v *Variable) inject(var_names []string, tmp interface{}, f func(interface{}, string) (interface{}, error)) (interface{}, error) {
 	name := var_names[0]
