56c22b8bf8a1d4eb8abdacffeecf92c61886879c
akm <akm2000@gmail.com>
2017-04-05 17:16:07 +0900

    :recycle: Extract configPath method

--- cli.go
+++ cli.go
@@ -27,18 +27,14 @@ func main() {
 }
 
 func run(c *cli.Context) error {
-	ctx := context.Background()
-
-	configPath := c.String("config")
-	if configPath == "" {
-		configPath = "./config.json"
-	}
-	config, err := LoadProcessConfig(configPath)
+	config, err := LoadProcessConfig(configPath(c))
 	if err != nil {
 		fmt.Printf("Error to load %v cause of %v\n", configPath, err)
 		os.Exit(1)
 	}
 	config.setup(os.Args[1:])
+
+	ctx := context.Background()
 
 	p := &Process{config: config}
 	err = p.setup(ctx)
@@ -53,4 +49,12 @@ func run(c *cli.Context) error {
 		os.Exit(1)
 	}
 	return nil
+}
+
+func configPath(c *cli.Context) string {
+	r := c.String("config")
+	if r == "" {
+		r = "./config.json"
+	}
+	return r
 }
