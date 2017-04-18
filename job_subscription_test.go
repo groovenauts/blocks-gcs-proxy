cc6ace2d4759b8a1779fd008ca2f19cf48f7e7ae
akm <akm2000@gmail.com>
2017-04-18 20:44:13 +0900

    :green_heart: Add test for JobSubscription.process

--- job_subscription_test.go
+++ job_subscription_test.go
@@ -88,3 +88,81 @@ func TestJobConfigSetupSustainer(t *testing.T) {
 	assert.Equal(t, float64(400), jc.Sustainer.Delay)
 	assert.Equal(t, float64(320), jc.Sustainer.Interval)
 }
+
+
+type DummyPuller struct {
+	responses []*pubsub.PullResponse
+}
+
+func (p *DummyPuller) Pull(subscription string, pullrequest *pubsub.PullRequest) (*pubsub.PullResponse, error) {
+	if len(p.responses) > 0 {
+		res := p.responses[0]
+		p.responses = p.responses[1:]
+		return res, nil
+	} else {
+		return nil, nil
+	}
+}
+func (p *DummyPuller) Acknowledge(subscription, ackId string) (*pubsub.Empty, error) {
+	return nil, nil
+}
+func (p *DummyPuller) ModifyAckDeadline(subscription string, ackIds []string, ackDeadlineSeconds int64) (*pubsub.Empty, error) {
+	return nil, nil
+}
+func (p *DummyPuller) Get(subscription string) (*pubsub.Subscription, error) {
+	return nil, nil
+}
+
+func TestJobSubscriptionProcess(t *testing.T) {
+	puller := &DummyPuller{
+		responses: []*pubsub.PullResponse{
+			&pubsub.PullResponse{
+				ReceivedMessages: []*pubsub.ReceivedMessage{
+					&pubsub.ReceivedMessage{
+						AckId: "dummy-ack-id1",
+						Message: &pubsub.PubsubMessage{
+							Attributes: map[string]string {
+								"foo": "A",
+							},
+							MessageId: "dummy-msg-id1",
+						},
+					},
+				},
+			},
+			&pubsub.PullResponse{
+				ReceivedMessages: []*pubsub.ReceivedMessage{},
+			},
+		},
+	}
+
+	jc := &JobConfig{
+		Subscription: "projects/dummy-proj-999/subscriptions/test01-job-subscription",
+		PullInterval: 10,
+		Sustainer: &JobSustainerConfig{
+			Delay:    600,
+			Interval: 480,
+		},
+	}
+	jc.setupSustainer(puller)
+
+	s := &JobSubscription{
+		config: jc,
+		puller: puller,
+	}
+
+	f := func(msg *JobMessage) error {
+		return nil
+	}
+	// MessageId: dummy-msg-id1
+	executed, error := s.process(f)
+	assert.True(t, executed)
+	assert.NoError(t, error)
+	// PullResponse is returned but any ReceivedMessage doesn't exist
+	executed, error = s.process(f)
+	assert.False(t, executed)
+	assert.NoError(t, error)
+	// PullResponse isn't returned
+	executed, error = s.process(f)
+	assert.False(t, executed)
+	assert.NoError(t, error)
+}
