2497bd4a23f1e2c69642cbc13d39edd5bd42135f
akm <akm2000@gmail.com>
2017-06-07 11:06:11 +0900

    :recycle: Extract puller.go from job_subscription.go

--- job_subscription.go
+++ job_subscription.go
@@ -8,42 +8,6 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-type (
-	Puller interface {
-		Pull(subscription string, pullrequest *pubsub.PullRequest) (*pubsub.PullResponse, error)
-		Acknowledge(subscription, ackId string) (*pubsub.Empty, error)
-		ModifyAckDeadline(subscription string, ackIds []string, ackDeadlineSeconds int64) (*pubsub.Empty, error)
-		Get(subscription string) (*pubsub.Subscription, error)
-	}
-
-	pubsubPuller struct {
-		subscriptionsService *pubsub.ProjectsSubscriptionsService
-	}
-)
-
-func (pp *pubsubPuller) Pull(subscription string, pullrequest *pubsub.PullRequest) (*pubsub.PullResponse, error) {
-	return pp.subscriptionsService.Pull(subscription, pullrequest).Do()
-}
-
-func (pp *pubsubPuller) Acknowledge(subscription, ackId string) (*pubsub.Empty, error) {
-	ackRequest := &pubsub.AcknowledgeRequest{
-		AckIds: []string{ackId},
-	}
-	return pp.subscriptionsService.Acknowledge(subscription, ackRequest).Do()
-}
-
-func (pp *pubsubPuller) ModifyAckDeadline(subscription string, ackIds []string, ackDeadlineSeconds int64) (*pubsub.Empty, error) {
-	req := &pubsub.ModifyAckDeadlineRequest{
-		AckDeadlineSeconds: ackDeadlineSeconds,
-		AckIds:             ackIds,
-	}
-	return pp.subscriptionsService.ModifyAckDeadline(subscription, req).Do()
-}
-
-func (pp *pubsubPuller) Get(subscription string) (*pubsub.Subscription, error) {
-	return pp.subscriptionsService.Get(subscription).Do()
-}
-
 type JobConfig struct {
 	Subscription string              `json:"subscription,omitempty"`
 	PullInterval int                 `json:"pull_interval,omitempty"`
