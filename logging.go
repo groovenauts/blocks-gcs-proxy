ffddcfc9a12ac20393f29e8d60811baf582fc9dc
akm <akm2000@gmail.com>
2017-08-17 14:07:38 +0900

    :+1: Add ProjectID and LogName to LoggingConfig

--- logging.go
+++ logging.go
@@ -1,11 +1,19 @@
 package main
 
 type LoggingConfig struct {
+	ProjectID       string						`json:"project_id"`
+	LogName		      string						`json:"log_name"`
 	Type            string            `json:"type"`
 	Labels          map[string]string `json:"labels"`
 }
 
 func (c *LoggingConfig) setup() *ConfigError {
+	if c.ProjectID == "" {
+		return &ConfigError{Name: "project_id", Message: "is required"}
+	}
+	if c.LogName == "" {
+		return &ConfigError{Name: "log_name", Message: "is required"}
+	}
 	if c.Type == "" {
 		return &ConfigError{Name: "type", Message: "is required"}
 	}
